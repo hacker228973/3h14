0684ec8e10674e4978e1d929dd3a55b4772692c1
hacker228973 <kodiy9@gmail.com>
2022-05-25 14:28:05 +0200

    Переделал 3

--- src/MyStringArrayList.java
+++ src/MyStringArrayList.java
@@ -9,12 +9,30 @@ public class MyStringArrayList {
         System.arraycopy(array, 0, newArray, 0, size);
         size = newLength;
         array = newArray;
+
+    }
+    private void resizeForRemove(int newLength) {
+        String[] newArray = new String[newLength];
+        System.arraycopy(array, 0, newArray, 0, newLength);
+        size = newLength;
+        array = newArray;
+
     }
 
     public void add(int index, String element) {
         if (rangeCheckFor(index,"add")) {
-            array[index] = element;
-            resize(size + 1);
+            if(array[index]==null) {
+                resize(size + 1);
+                array[index] = element;
+            }
+            else{
+                resize(size + 1);
+                for(int i = size-1;i>index;i--){
+
+                    array[i]=array[i-1];
+                }
+                array[index] = element;
+            }
         }
 
     }
@@ -22,7 +40,8 @@ public class MyStringArrayList {
     public void remove(int index) {
         if (rangeCheckFor(index,"remove")){
             if (size - 1 - index >= 0) System.arraycopy(array, index + 1, array, index, size - 1 - index);
-            array[size - 1] = null;
+//            array[size - 1] = null;
+            resizeForRemove(size-1);
         }
     }
 
@@ -34,6 +53,8 @@ public class MyStringArrayList {
 
     }
 
+
+
     public void get(int index) {
         if (rangeCheckFor(index,"get")) {
             System.out.println(array[index]);
@@ -44,13 +65,16 @@ public class MyStringArrayList {
         if (index >= 0 && index < size) {
             return true;
         }
-        System.out.println("sry, but u cant "+ action +" the element, cause index under number "+ index +" exist from the size of the Array, maximum value is " +(size-1));
+        System.out.println("sry, but you can't "+ action +" the element, cause index under number "+ index +" exist from the size of the Array, maximum value is " +(size-1));
         return false;
     }
 
 
     @Override
     public String toString() {
-        return "MyArrayList=" + Arrays.toString(array);
+        String[] newArray = new String[size-1];
+        System.arraycopy(array, 0, newArray, 0, size-1);
+        return "MyArrayList=" + Arrays.toString(newArray);
+
     }
 }
